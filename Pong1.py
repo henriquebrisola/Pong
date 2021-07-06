ace6ea64145c117825674c172fd048a26aca48d0
Henrique Brisola <henriquebrisola@gmail.com>
2021-07-06 15:15:38 -0300

    first commit

---
+++ Pong1.py
@@ -0,0 +1,16 @@
+
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Hick")
+wn.bgcolor("#000030")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+# Main game loop
+
+while True:
+    wn.update()
